bac5a8d8be82e6f6eb5d778bafb5a702f9b56409
ryuuk96 <ajayadi.nair@gmail.com>
2019-10-22 11:20:39 +0100

    Created a basic structure of the project along with an exe to test the application. Also creating unit tests for testing the application

---
+++ Logger/Validator.cs
@@ -0,0 +1,19 @@
+﻿namespace Logger
+{
+    public class Validator
+    {
+        public ValidationResult Validate(object expected, object actual)
+        {
+            if (!expected.GetType().Name.Equals(actual.GetType().Name))
+                return ValidationResult.InvalidCheck;
+            return ValidationResult.Passed;
+        }
+
+        public enum ValidationResult
+        {
+            Passed,
+            Failed,
+            InvalidCheck
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Logger
+ This project is being built to create a generic logger. This can be used to log details into multiple formats such as csv/xml. The logger must be built smart enough to create logs regarding the class that is calling function to log details along with current date and time needed to logs details in the desired format.
